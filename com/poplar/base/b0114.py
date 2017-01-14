7ea70412d82c03d245e13a820cf16114daff2af3
Poplar Tang <poplar_tang@163.com>
2017-01-14 09:26:31 +0800

    string

---
+++ com/poplar/base/b0114.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+names=['Job','Favourite','Thinking']
+print len(names)
+print names[-1] #打印倒数第一个
+names.append('Append')
+print names[-1]
+names.pop()
+print names
+names.insert(0, 'First')
+print names[0]
