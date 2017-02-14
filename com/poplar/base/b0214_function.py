fd44742d84f105817fbcf71b13210cf14ee8ab8c
Ty <339402908@qq.com>
2017-02-14 23:10:45 +0800

    函数定义

---
+++ com/poplar/base/b0214_function.py
@@ -0,0 +1,13 @@
+def my_abs(x):
+    if x >= 0:
+        return x
+    else:
+        return -x;
+
+def myAbs(x):
+    if not isinstance(x (int, float)):
+        raise TypeError('bad operand type')
+    if x >= 0:
+        return x
+    else:
+        return -x
